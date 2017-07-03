8e102a7d7279b9747fa74b1f9fb8baad42d81b3b
emanuelsamel <mannys94@hotmail.com>
2017-07-03 16:58:09 -0400

    js run pass

---
+++ javascript_warm_up.js
@@ -0,0 +1,43 @@
+// var apples = 14
+// console.log('I have ' + apples + ' apples.');
+//
+// var materials = ['wood', 'metal', 'stone'];
+//
+// var words = {
+//   elephant: "The world's largest land mammal.",
+//   school: "A place of learning.",
+//   iceCream: "A delicious milk-based dessert."
+// }
+//
+// var num = 10
+//
+// if(num > 10){
+//   console.log(num + ' is greater than 10.');
+// } else if (num === 10) {
+//   console.log( num + ' is exactly 10.');
+// }else{
+//   console.log( num + ' must be less than 10.');
+// }
+//
+// for (var i = 0; i < 10; i++) {
+//   console.log("Doing the same thing over and over.");
+// }
+// var base = 5
+// for (var i = 0; i < 20; i++){
+//   console.log(i += base);
+// }
+
+// var total = 0
+//
+// for (var num = 0; num < 100; num++){
+// total += num;
+// }
+// console.log(total)
+
+// for (var height = 3; height < 15; height++ ){
+//   if(height > 9){
+//     console.log('You can get on the rollercoaster!');
+//   }else{
+//     console.log("You are too short to ride this rollercoaster.");
+//   }
+// }
